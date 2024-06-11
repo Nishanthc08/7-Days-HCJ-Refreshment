4a49682732dffc3dfa90ca2fe12fba650ac27add
Spartanc08 <49616658+Nishanthc08@users.noreply.github.com>
2024-06-11 20:26:06 +0530

    Add files via upload

---
+++ Day 5/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const addToCartButton = document.getElementById('add-to-cart');
+
+    addToCartButton.addEventListener('click', () => {
+        alert('Product added to cart!')
+    });
+});
